a324de8b13a89ae9373eac54793ea19df5f3c2b0
Sombeet K Sahu <sombeet@gmail.com>
2014-12-16 21:52:31 -0800

    Working Build

--- BaseLine_Comparision_Script/folder_Struct.py
+++ BaseLine_Comparision_Script/folder_Struct.py
@@ -15,8 +15,8 @@ Options:
     Compare two directories recursively. Files in each directory are assumed to be
     equal if their names and contents are equal.
 
-    @param dir1: First directory path     #CURRENTLY HARDCODED WILL TAKE ARGUMENT LATER
-    @param dir2: Second directory path    #CURRENTLY HARDCODED WILL TAKE ARGUMENT LATER
+    @param dir1: First directory path
+    @param dir2: Second directory path
 
 Output:
     Prints if the file tree is same and  there were no errors while accessing the directories or files
@@ -25,21 +25,21 @@ Output:
 
 import filecmp
 import os.path
-#import os
+import sys
 
 def compare_directory(dir1, dir2):
 
     dirctry = filecmp.dircmp(dir1, dir2)
-    print dirctry.report()
+    # print dirctry.report()
+
     if len(dirctry.left_only)>0 or len(dirctry.right_only)>0 or \
         len(dirctry.funny_files)>0:
-        #print False
         return False
 
     (match, mismatch, errors) =  filecmp.cmpfiles(
         dir1, dir2, dirctry.common_files, shallow=False)
-    print match
-    #print mismatch
+
+    print mismatch
     if len(mismatch)>0 or len(errors)>0:
         return False
     for common_dir in dirctry.common_dirs:
@@ -51,12 +51,11 @@ def compare_directory(dir1, dir2):
 
 
 def main():
-    # DIR = os.path.dirname(os.path.realpath(__file__))
-    DIR = "C:\Users\sahus3\Desktop\dat1"
-    DIR2 = "C:\Users\sahus3\Desktop\dat2"
-    dr = compare_directory(DIR, DIR2)
-    #print dr
 
+    DIR = sys.argv[1]
+    DIR2 = sys.argv[2]
+
+    dr = compare_directory(DIR, DIR2)
     if dr is True:
         print "The files are same in both directories"
     else:
